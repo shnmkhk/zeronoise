8392378405037be1ce5ff6b00f8c7ba4319ed881
Shanmukha Katuri <shanmukha.k@gmail.com>
2020-05-28 16:28:05 +0530

    shnmkhk: Added Play/ Stop functionality

--- src/main/java/com/rabbit/zeronoise/Main.java
+++ src/main/java/com/rabbit/zeronoise/Main.java
@@ -4,6 +4,8 @@ import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineEvent;
+import javax.sound.sampled.LineListener;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.TargetDataLine;
 
@@ -22,11 +24,12 @@ import javafx.stage.WindowEvent;
 public class Main extends Application {
 	private AudioFormat audioFormat;
 	private TargetDataLine targetDataLine;
+	private Clip currentPlayingClip;
 
 	public void start(Stage primaryStage) {
 		final Button startButton = new Button("Start");
 		final Button stopButton = new Button("Stop");
-		final Button playButton = new Button("Play");
+		final Button playOrStopButton = new Button("Play/ Stop");
 
 		startButton.setLayoutX(40);
 		startButton.setLayoutY(40);
@@ -36,16 +39,15 @@ public class Main extends Application {
 		stopButton.setLayoutY(40);
 		stopButton.setDisable(true);
 
-		playButton.setLayoutX(160);
-		playButton.setLayoutY(40);
-		playButton.setDisable(false);
+		playOrStopButton.setLayoutX(160);
+		playOrStopButton.setLayoutY(40);
+		playOrStopButton.setDisable(false);
 
 		startButton.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent e) {
 				startButton.setDisable(true);
 				stopButton.setDisable(false);
-				playButton.setDisable(true);
 
 				recordAudio();
 			}
@@ -56,25 +58,27 @@ public class Main extends Application {
 			public void handle(ActionEvent e) {
 				startButton.setDisable(false);
 				stopButton.setDisable(true);
-				playButton.setDisable(false);
 
 				targetDataLine.stop();
 				targetDataLine.close();
 			}
 		});
 
-		playButton.setOnAction(new EventHandler<ActionEvent>() {
+		playOrStopButton.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				playButton.setDisable(true);
-				playAudio();
-				playButton.setDisable(false);
+				if (currentPlayingClip != null) {
+					currentPlayingClip.stop();
+					currentPlayingClip = null;
+				} else {
+					currentPlayingClip = playAudio();
+				}
 			}
 		});
 
 		Pane pane = new Pane();
 		Text text = new Text(20, 20, "Voice Recorder");
-		pane.getChildren().addAll(text, startButton, stopButton, playButton);
+		pane.getChildren().addAll(text, startButton, stopButton, playOrStopButton);
 
 		Scene scene = new Scene(pane, 400, 100);
 		primaryStage.setTitle("Zero noise");
@@ -105,17 +109,14 @@ public class Main extends Application {
 		}
 	}
 
-	public void playAudio() {
+	public Clip playAudio() {
 		try {
 			PlayingThread pt = new PlayingThread();
-			Clip clip = pt.call();
-			while (true) {
-				if (!clip.isRunning())
-					break;
-			}
+			return pt.call();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		return null;
 	}
 
 	public static void main(String[] args) {
