7da6a6d21ff29fda98f09478fa70278f59b90067
Katuri <Shanmukha.Katuri@parexel.com>
2020-06-29 11:11:57 +0530

    shnmkhk: Incremented version to 1.3 and added a detailed logging for deleted recordings

--- src/main/java/com/rabbit/zeronoise/Main.java
+++ src/main/java/com/rabbit/zeronoise/Main.java
@@ -1,8 +1,11 @@
 package com.rabbit.zeronoise;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Optional;
+import java.util.prefs.Preferences;
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
@@ -18,9 +21,17 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -34,20 +45,37 @@ public class Main extends Application {
 	private AudioClip currentPlayingClip;
 
 	public void start(Stage primaryStage) {
-		final Button startButton = new Button("Start");
-		final Button stopButton = new Button("Stop");
+		final Button startButton = new Button("Record (R)");
+		final Button stopButton = new Button("Stop (S)");
+		final Button playLatestButton = new Button("Play latest record (P)");
 		final Label statusLabel = new Label("Ready");
 		final ListView<String> listView = new ListView<String>();
 		final VBox vbox = new VBox(15, listView);
 		
-		startButton.setLayoutX(40);
+		startButton.setLayoutX(20);
 		startButton.setLayoutY(40);
 		startButton.setDisable(false);
 
-		stopButton.setLayoutX(100);
+		stopButton.setLayoutX(130);
 		stopButton.setLayoutY(40);
 		stopButton.setDisable(true);
-
+		
+		playLatestButton.setLayoutX(220);
+		playLatestButton.setLayoutY(40);
+		playLatestButton.setDisable(false);
+		
+		listView.setOnKeyReleased(new EventHandler<KeyEvent>() {
+			public void handle(KeyEvent event) {
+				if (!event.getCode().equals(KeyCode.ENTER) && 
+						!event.getCode().equals(KeyCode.SPACE)) return;
+				if (currentPlayingClip != null && currentPlayingClip.isPlaying()) {
+					currentPlayingClip.stop();
+				}
+				final String fileNameToPlay = listView.getSelectionModel().getSelectedItem() + ".wav";
+				playAudio(fileNameToPlay);
+			};
+		});
+		
 		listView.setOnMouseClicked(new EventHandler<MouseEvent>() {
 
 			@Override
@@ -91,14 +119,27 @@ public class Main extends Application {
 				refreshRecordingsList(listView);
 			}
 		});
+		
+		playLatestButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				listView.getSelectionModel().select(0);
+				if (currentPlayingClip != null && currentPlayingClip.isPlaying()) {
+					currentPlayingClip.stop();
+				}
+				final String fileNameToPlay = listView.getSelectionModel().getSelectedItem() + ".wav";
+				playAudio(fileNameToPlay);
+			}
+		});
 
 		Pane pane = new Pane();
 		Text text = new Text(20, 20, "Voice Recorder");
-		pane.getChildren().addAll(text, startButton, stopButton, vbox);
-
+		pane.getChildren().addAll(text, startButton, stopButton, playLatestButton, vbox);
+		
 		Scene scene = new Scene(pane, 400, 520);
-		primaryStage.setTitle("Zero noise");
+		primaryStage.setTitle("Zeronoise - Voice recorder");
 		primaryStage.setScene(scene);
+		primaryStage.getIcons().add(new Image(Thread.currentThread().getContextClassLoader().getResourceAsStream("record.png")));
 		primaryStage.show();
 
 		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
@@ -107,6 +148,79 @@ public class Main extends Application {
 				Platform.exit();
 			}
 		});
+		
+		KeyCombination R_Record_Key = new KeyCodeCombination(KeyCode.R);
+		KeyCombination S_Stop_Key = new KeyCodeCombination(KeyCode.S);
+		KeyCombination P_Play_Latest_Recording_Key = new KeyCodeCombination(KeyCode.P);
+		KeyCombination CTRL_R_Reset_Sequence_Key = new KeyCodeCombination(KeyCode.R, KeyCombination.CONTROL_DOWN);
+		KeyCombination CTRL_D_Reset_Sequence_Key = new KeyCodeCombination(KeyCode.D, KeyCombination.CONTROL_DOWN);
+		scene.getAccelerators().put(R_Record_Key, ()-> {
+			startButton.setDisable(true);
+			stopButton.setDisable(false);
+
+			recordAudio();
+		});		
+		
+		scene.getAccelerators().put(S_Stop_Key, ()-> {
+			startButton.setDisable(false);
+			stopButton.setDisable(true);
+
+			targetDataLine.stop();
+			targetDataLine.close();
+
+			refreshRecordingsList(listView);
+		});		
+		
+		scene.getAccelerators().put(P_Play_Latest_Recording_Key, ()-> {
+			listView.getSelectionModel().select(0);
+			if (currentPlayingClip != null && currentPlayingClip.isPlaying()) {
+				currentPlayingClip.stop();
+			}
+			if (listView.getSelectionModel().getSelectedItem() == null) return;
+			final String fileNameToPlay = listView.getSelectionModel().getSelectedItem() + ".wav";
+			playAudio(fileNameToPlay);
+		});		
+		
+		scene.getAccelerators().put(CTRL_R_Reset_Sequence_Key, ()-> {
+			Preferences prefs = Preferences.userRoot().node(ZConstants.PREF_ROOT_NODE_NAME_ZERONOISE_MEDIA);
+			prefs.putInt(ZConstants.PREF_KEY_PREV_SEQUENCE, 0);
+			System.out.println("Sequence has been reset to zero");
+		});	
+		
+		scene.getAccelerators().put(CTRL_D_Reset_Sequence_Key, ()-> {
+			File recordingsDir = new File(ZConstants.SYSTEM_TEMP_DIRECTORY + File.separatorChar + "zeronoise");
+			if (!recordingsDir.exists()) {
+				System.out.println("No records found to delete");
+			}
+			Alert deleteRecsAlert = new Alert(AlertType.CONFIRMATION);
+			deleteRecsAlert.setContentText("Are you sure you want to delete all the previous recordings");
+			Optional<ButtonType> result = deleteRecsAlert.showAndWait();
+			if (result.get() == ButtonType.OK){
+				// Lists all files in folder
+				File fList[] = recordingsDir.listFiles(new FilenameFilter() {
+					@Override
+					public boolean accept(File dir, String name) {
+						if (name.endsWith(".wav")) {
+							return true;
+						}
+						return false;
+					}
+				});
+				// Searchs .lck
+				int filesDeletedCount = 0;
+				for (int i = 0; i < fList.length; i++) {
+					File record = fList[i];
+					if (!record.exists()) continue;
+				    if (!fList[i].delete()) System.out.println("Unable to delete: " + record.getName());
+				    else ++filesDeletedCount;
+				}
+				refreshRecordingsList(listView);
+				System.out.println(filesDeletedCount + " recording(s) have been deleted");
+			} else {
+				System.out.println("User cancelled the request");
+			}
+		});	
+		
 	}
 	
 	private void refreshRecordingsList(ListView<String> listView) {
--- src/main/java/com/rabbit/zeronoise/util/ZConstants.java
+++ src/main/java/com/rabbit/zeronoise/util/ZConstants.java
@@ -4,6 +4,10 @@ public class ZConstants {
 
 	public static final String PREF_KEY_PREV_SEQUENCE = "ZERONOISE_PREVIOUS_SEQUENCE";
 	public static final String PREF_ROOT_NODE_NAME_ZERONOISE_MEDIA = "ZERONOISE_MEDIA";
-	public static final String TEMP_RECORDING_FILE = "#sequence#_zeronoise_recording.wav";
+	public static String TEMP_RECORDING_FILE = "#sequence#_zeronoise_recording.wav";
+	
+	static {
+		TEMP_RECORDING_FILE = "#sequence#_"+System.getProperty("user.name")+"_recording.wav";
+	}
 	public static final String SYSTEM_TEMP_DIRECTORY = System.getProperty("java.io.tmpdir");
 }
